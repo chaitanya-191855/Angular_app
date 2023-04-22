fe4363281808f24a2f1189a72126a4b4037d08d5
saichaitanya.raavi <saichaitanya.raavi@apxor.com>
2023-04-22 16:31:58 +0530

    Contact page edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
 import { FooterComponent } from './sharepage/footer/footer.component';
@@ -10,6 +11,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenupageComponent } from './pages/menupage/menupage.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { MenupageComponent } from './pages/menupage/menupage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+  
 export class ContactComponent {
+  constructor(private router: Router) { }
+  async sendEmail(e: Event) {
+    e.preventDefault()
+    emailjs.sendForm('service_a9y4g2b', 'template_hchc1py', e.target as HTMLFormElement, 'qXG5XVRFXE6inUn93')
+      .then((result: EmailJSResponseStatus) => {
+        alert("successfull filled")
+        this.router.navigateByUrl('');
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+ }
 
-}
